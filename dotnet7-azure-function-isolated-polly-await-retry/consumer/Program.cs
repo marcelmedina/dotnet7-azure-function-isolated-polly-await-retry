31cb8adb5fb0f7b282db565f1355aeeccffd233a
Marcel Medina <marcel.net@gmail.com>
2023-09-01 13:53:15 +1200

    fotnet7-azure-function-polly-await-and-retry

---
+++ dotnet7-azure-function-isolated-polly-await-retry/consumer/Program.cs
@@ -0,0 +1,39 @@
+using consumer.TypedHttpClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        var currentDirectory = hostingContext.HostingEnvironment.ContentRootPath;
+
+        config.SetBasePath(currentDirectory)
+            .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+        config.Build();
+    })
+    .ConfigureServices((services) =>
+    {
+        var retryPolicy = Policy
+            .HandleResult<HttpResponseMessage>(response => !response.IsSuccessStatusCode)
+            .WaitAndRetryAsync(3,
+                _ => TimeSpan.FromMilliseconds(1000),
+                onRetry: (message, retryCount) =>
+                {
+                    Console.Out.WriteLine("----------------------------------------------------");
+                    Console.Out.WriteLine($"### RequestMessage: {message.Result.RequestMessage}");
+                    Console.Out.WriteLine($"### StatusCode: {message.Result.StatusCode}");
+                    Console.Out.WriteLine($"### ReasonPhrase: {message.Result.ReasonPhrase}");
+                    Console.Out.WriteLine($"### Retry: {retryCount}");
+                    Console.Out.WriteLine("----------------------------------------------------");
+                });
+
+        services.AddHttpClient<StateCounterHttpClient>()
+            .AddPolicyHandler(retryPolicy);
+    })
+    .Build();
+
+host.Run();
